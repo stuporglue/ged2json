df7547c9e0809dae2fa30cbc0c7945ff5a8adb7f
Michael Moore <stuporglue@gmail.com>
2013-05-28 22:53:17 -0500

    Fixed some geometry issues

--- examples/php/lib/ged2geojson.php
+++ examples/php/lib/ged2geojson.php
@@ -24,19 +24,19 @@ class ged2geojson extends ged2json{
         foreach($ancestors as $ancestorId => $ancestor){
             if(array_key_exists('events',$ancestor)){
                 if($refPlace = $this->getRefPlace($ancestor['events'])){
-                    $ancestor['refplace'] = $refPlace;
+                    $ancestor['refplace'] = $refPlace['geo']['geometry'];
                 }
             }
 
             $feature = Array(
                 'id' => $ancestor['id'],
+                'type' => 'Feature',
+                'geometry' => NULL,
                 'properties' => $ancestor
             );
 
-            if(array_key_exists('refplace',$ancestor) && array_key_exists('geo',$ancestor['refplace'])){
-                $feature['geometry'] = $ancestor['refplace']['geo'];
-            }else{
-                $feature['geometry'] = NULL;
+            if(array_key_exists('refplace',$ancestor)){
+                $feature['geometry'] = $ancestor['refplace'];
             }
 
             $geojson['features'][] = $feature;
@@ -68,8 +68,8 @@ class ged2geojson extends ged2json{
         }
 
         $places = array_unique($places);
-        require('ssgeocoder/ssgeocoder.php');
-        $geocoder = new ssgeocoder('/tmp/test.sqlite');
+        require('ssgeocoder.php');
+        $geocoder = new ssgeocoder();
         $geoplaces = $geocoder->geocode($places);
 
         foreach($ancestors as $ancestorId => $ancestor){
